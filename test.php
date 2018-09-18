3a76375397bce3d09f2f58f57ef467ca15fe04b0
jatin06 <jatin.yadav142@gmail.com>
2018-09-18 14:17:01 +0530

    testing

--- test.php
+++ test.php
@@ -4,4 +4,10 @@ adding more lines to see the working
 line4
 line5
 line6
+<<<<<<< HEAD
 adding few more lines to the code 
+=======
+make changes
+remind me the cmds that are useful in git to make 
+it work properly  
+>>>>>>> test1
