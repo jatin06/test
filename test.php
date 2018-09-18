481422e9a54ea2026dc3d6f39daaa44827748ee9
jatin06 <jatin.yadav142@gmail.com>
2018-09-18 11:40:46 +0530

    new functios

--- test.php
+++ test.php
@@ -1,4 +1,6 @@
 print "started to learn git"
 modify the file to check the git funcationalities
-adding mor lines to see the working
-line4 
+adding more lines to see the working
+line4
+line5
+line6 
